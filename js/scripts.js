911348edc09dc99fec418ccce8b4921289523d3e
Maarg2022 <116040849+Maarg2022@users.noreply.github.com>
2022-11-06 10:43:39 -0600

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
-//guardo display,  de html en una variable cada uno
+//guardo display de html en una variable cada uno
 let display = document.getElementById('display');
 
-//covirtiendo la coleccion de html en un array que recorre todos los btones
+//covirtiendo la coleccion de html en un array que recorre todos los botones
 let buttons = Array.from(document.getElementsByClassName('button'));
 
 //debbuging
@@ -12,20 +12,20 @@ buttons.map(button => {
     button.addEventListener('click', (e) => {
         switch(e.target.innerText){
             case 'C':
-                display.innerText = '';//limpia 
-                break;
+                display.innerText = ''; 
+                break;//limpia
             case '←':
-                if(display.innerText){// la condicion me permite solo remover el ultimo caracter
+                if(display.innerText){// me permite solo remover el ultimo caracter
                     display.innerText = display.innerText.slice(0, -1);
                 }
-                break; // evita que me diaplay este signo en la calc
+                break; // evita que me escriba este signo (←) en la calc
             case '=':
                 try{ //
                     display.innerText = eval(display.innerText);
                 } catch{
                     display.innerText = 'Error!';
-                } //en caso de que esten entrado datos erroneos   
-                break; // me permite no presenta el signo = en la pantalla
+                } //en caso de datos erroneos   
+                break; // me permite no presentar el signo = en la pantalla
             default:
                 display.innerText += e.target.innerText;// display cada tecla presionada
         }
